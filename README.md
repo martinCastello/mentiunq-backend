d3b54e25981a5ae5789e0573c6633e3bb4ac485c
mcastello <mcastello@nacionservicios.com.ar>
2022-08-31 19:59:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mentiunq-backend
---
+++ src/main/java/ar/edu/unq/pdes/mentiunq/MentiunqApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.pdes.mentiunq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentiunqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentiunqApplication.class, args);
+	}
+
+}
